b52bc65e14073d778526c05c3ae908868ab9fa20
Massay Bah <massaybah@gmail.com>
2024-12-21 12:13:21 +0000

    latest changes

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
+import { Link } from '@inertiajs/vue3';
 
 defineProps({
     items: Array
@@ -17,32 +18,42 @@ defineProps({
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                           
-                            <table class="w-full text-center">
-                                  <thead class="w-full">
-                                      <tr>
-                                        <th class="border">Type</th>
-                                        <th class="border">Sale Id</th>
-                                        <th class="border">Station</th>
-                                        <th class="border">Reference</th>
-                                        <th class="border">Date</th>
-                                        <th class="border">User</th>
-                                        <th class="border">Created_at</th>
-                                      </tr>
-                                  </thead>
 
-                                  <tbody>
-                                    <tr v-for="(row,index) in items.data" :key="index">
-                                        <td class="border">{{  row.type }}</td>
-                                        <td class="border">{{  row.sale_id }}</td>
-                                        <td class="border">{{  row.station ? row.station.name : '-'  }}</td>
-                                        <td class="border">{{  row.reference }}</td>
-                                        <td class="border">{{  row.date }}</td>
-                                        <td class="border">{{  row.user.email }}</td>
-                                        <td class="border">{{  row.created_at }}</td>
-                                    </tr>
-                                  </tbody>
-                            </table>
+                    <table class="w-full text-center">
+                        <thead class="w-full">
+                            <tr>
+                                <th class="border">Type</th>
+                                <th class="border">Sale Id</th>
+                                <th class="border">Station</th>
+                                <th class="border">Reference</th>
+                                <th class="border">Date</th>
+                                <th class="border">User</th>
+                                <th class="border">Created_at</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="(row, index) in items.data" :key="index">
+                                <td class="border">{{ row.type }}</td>
+                                <td class="border">{{ row.sale_id }}</td>
+                                <td class="border">{{ row.station ? row.station.name : '-' }}</td>
+                                <td class="border">{{ row.reference }}</td>
+                                <td class="border">{{ row.date }}</td>
+                                <td class="border">{{ row.user.email }}</td>
+                                <td class="border">{{ row.created_at }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <!-- {{ items.links}} -->
+                    <div class="flex  justify-center gap-2 items-center p-2">
+                        <template v-for="(link, index) in items.links" :key="index">
+                            <span v-if="link.url == null" class="bg-gray-100 rounded-md p-2 font-extrabold" :class="{'bg-emerald-700 text-gray-100': link.active}">
+                                <span v-html="link.label"/>
+                            </span>
+                            <Link  v-else  :href="link.url" class="bg-emerald-400 text-center p-2 rounded-md font-extrabold" :class="{'bg-emerald-700 text-gray-100': link.active}">
+                                    <span v-html="link.label"/>
+                            </Link>
+                        </template>
+                    </div>
                 </div>
             </div>
         </div>
